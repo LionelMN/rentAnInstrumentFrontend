f3143b88a861a0296b0d5a2e122bf2a9ba519fb7
Lionel Montesdeoca <lionelmontesdeocanuez.com>
2021-11-21 16:30:35 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostBinding, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rentAnInstrumentFrontend';
+export class AppComponent implements OnInit{
+/*   private isDark : boolean = false;
+
+  @HostBinding('class')
+  get themeMode(){
+    return this.isDark ? 'theme-dark' : 'theme-light';
+  } */
+
+  constructor(@Inject(DOCUMENT) private document : Document, private renderer : Renderer2){}
+
+  ngOnInit() {
+    this.renderer.setAttribute(this.document.body, 'class', 'theme-light');
+  }
+
+  switchMode(isDarkMode : boolean){
+    const hostClass = isDarkMode ? 'theme-dark' : 'theme-light';
+    this.renderer.setAttribute(this.document.body, 'class', hostClass);
+  }
 }
